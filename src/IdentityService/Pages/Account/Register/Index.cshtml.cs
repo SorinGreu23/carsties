83ad0d366a7c5f0c03bf8fe1a69c73909adb7221
Sorin Greu <greusorin2003@gmail.com>
2024-04-27 13:18:56 +0300

    Identity Service - WIP

---
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Account.Register;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
