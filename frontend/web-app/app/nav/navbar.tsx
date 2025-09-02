ff998e24329690125dce65d9b6cc272164300ef0
Sorin Greu <greusorin2003@gmail.com>
2025-09-02 23:37:52 +0300

    Add auction filters and search functionality

--- frontend/web-app/app/nav/navbar.tsx
+++ frontend/web-app/app/nav/navbar.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { FaCar } from 'react-icons/fa6'
+import React from 'react';
+import Search from './Search';
+import Logo from './Logo';
 
 export default function Navbar() {
     return (
-        <header className='sticky top-0 z-50 flex justify-between bg-white p-5 item-center text-gray-800 shadow-md'>
-            <div className='flex items-center gap-2 text-2xl font-semibold text-red-500'>
-              <FaCar size={25}/>
-              <div>Carsties Auctions</div>
-            </div>
-            <div>Middle</div>
-            <div>Right</div>
-        </header>
-    )
+      <header className="sticky top-0 z-50 flex justify-between bg-white p-5 item-center text-gray-800 shadow-md">
+        <Logo />
+        <Search />
+        <div>Login</div>
+      </header>
+    );
 }   
